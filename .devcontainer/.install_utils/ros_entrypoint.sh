0751849f01a935f527dfbd1d903421d568808417
Brojoon-10 <137401659+Brojoon-10@users.noreply.github.com>
2026-03-31 01:17:14 +0900

    Update ros_entrypoint.sh to source ROS setup
    Source ROS setup to include PYTHONPATH for genmsg.

--- .devcontainer/.install_utils/ros_entrypoint.sh
+++ .devcontainer/.install_utils/ros_entrypoint.sh
@@ -2,4 +2,10 @@
 
 set -e
 
+### HJ : source ROS setup to ensure PYTHONPATH includes genmsg etc.
+source /opt/ros/noetic/setup.bash
+if [ -f /home/${USER}/catkin_ws/devel/setup.bash ]; then
+    source /home/${USER}/catkin_ws/devel/setup.bash
+fi
+
 exec "$@"
